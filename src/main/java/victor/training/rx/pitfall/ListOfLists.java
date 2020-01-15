c5ca8150e4b3420839c9a1d89c73c4081f82c797
Victor Rentea <victorrentea@gmail.com>
2020-01-15 19:43:10 +0200

    ListOfLists UNDONE

--- src/main/java/victor/training/rx/pitfall/ListOfLists.java
+++ src/main/java/victor/training/rx/pitfall/ListOfLists.java
@@ -6,12 +6,11 @@ import static java.util.Arrays.asList;
 
 public class ListOfLists {
     public static void main(String[] args) {
-        // TODO preserve the structure: I want a List<List> at the end
-        Observable.just(asList(1,2), asList(3,4))
-            .flatMap(list -> Observable.from(list)
+        // TODO preserve the structure: I want  [[2,4],[6,8]] at the end
+        Observable.just(asList(1, 2), asList(3, 4))
+                .flatMap(list -> Observable.from(list))
                 .map(n -> n * 2)
                 .toList()
-            )
-            .subscribe(System.out::println);
+                .subscribe(System.out::println);
     }
 }
