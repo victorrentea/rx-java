68c0444494ac623b49e2a0de11f05788b1c6ef19
Victor Rentea <victorrentea@gmail.com>
2020-01-15 19:23:08 +0200

    .toList UNDONE

--- src/main/java/victor/training/rx/pitfall/ToListTrap.java
+++ src/main/java/victor/training/rx/pitfall/ToListTrap.java
@@ -8,16 +8,11 @@ public class ToListTrap {
     public static void main(String[] args) {
 
         PublishSubject<String> p = PublishSubject.create();
-        p.toList().subscribe(System.out::println); // NEVER EMITS
-
-        p.scan(new ArrayList<String>(), (list, e) -> {list.add(e);return list;})
-                .subscribe(System.out::println);
+        p/*.toList()*/.subscribe(System.out::println);
 
         p.onNext("a");
         p.onNext("b");
 
-
-
-//        p.onCompleted(); // what if...
+        p.onCompleted(); // what if...
     }
 }
