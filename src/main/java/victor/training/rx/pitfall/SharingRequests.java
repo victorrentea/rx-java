9d48d225ea049ff29faefcdfede0fa56ee6c8ebc
Victor Rentea <victorrentea@gmail.com>
2020-01-15 19:33:18 +0200

    SharingRequests DONE

--- src/main/java/victor/training/rx/pitfall/SharingRequests.java
+++ src/main/java/victor/training/rx/pitfall/SharingRequests.java
@@ -13,43 +13,30 @@ public class SharingRequests {
     public static void main(String[] args) {
         log.debug("Start");
 
-        Observable<String> call = httpCallObs();
+        System.out.println("===ConnectableObserver===");
+        Observable<String> call = getFromHttp();
         ConnectableObservable<String> birouDeschis = call.publish();
         // TODO avoid doing two expensive network calls
         birouDeschis.subscribe(v -> System.out.println("Pun in cas: " + v));
-        // aici mai 1 ns
         birouDeschis.subscribe(v-> System.out.println("Dau aplicatiei"));
         birouDeschis.connect(); // abia acum observable-ul incepe sa emita
-        // .connect() poate fi enervant.
         birouDeschis.subscribe(v-> System.out.println("Dau aplicatiei 3 = NU RULEAZA"));
 
-
-        System.out.println("===========2==================");
-
-        Observable<String> pornesteAutomatCandAre2 = httpCallObs().publish().autoConnect(2);
-        // TODO avoid doing two expensive network calls
+        System.out.println("===autoConnect===");
+        Observable<String> pornesteAutomatCandAre2 = getFromHttp().publish().autoConnect(2);
         pornesteAutomatCandAre2.subscribe(v -> System.out.println("Pun in cas: " + v));
-        // aici mai 1 ns
         pornesteAutomatCandAre2.subscribe(v -> System.out.println("Dau aplicatiei"));
         pornesteAutomatCandAre2.subscribe(v -> System.out.println("Dau aplicatiei 3  = NU RULEAZA"));
 
+        System.out.println("===encapsulatedCache:chainObservers===");
+        // PROBLEM: launches the first http request before .subscribe is called here.
+        getFromHttpOrCache().subscribe(System.out::println);
+
+        System.out.println("===.defer to fix above===");
+        Observable.defer(() -> getFromHttpOrCache());// correct ugly solution
 
 
-
-        System.out.println("===========3==================");
-
-
-        getHttpSauCache().subscribe(System.out::println);
-
-        System.out.println("===========4==================");
-
-
-        getHttpSauCache();
-        Observable.defer(() -> getHttpSauCache());// -- posibila solutie (urata)
-
-
-
-        System.out.println("===========timp==================");
+        System.out.println("===Time Observable that doesn't restart on subscribe===");
         Observable<Long> timp = Observable.interval(100, TimeUnit.MILLISECONDS).share();
         timp.subscribe(System.out::println);
         ConcurrencyUtil.sleep(1000);
@@ -57,10 +44,10 @@ public class SharingRequests {
         ConcurrencyUtil.sleep(2000);
     }
 
-    private static Observable<String> getHttpSauCache() {
+    private static Observable<String> getFromHttpOrCache() {
         // PROBLEMA: iti face HTTP req chiar daca observableul intors nu e subscris de nimeni.
         BehaviorSubject<String> subject = BehaviorSubject.create();
-        httpCallObs()
+        getFromHttp()
                 .subscribe(v -> {
                     System.out.println("punInCache " + v);
                     subject.onNext(v);
@@ -72,7 +59,7 @@ public class SharingRequests {
 
     }
 
-    private static Observable<String> httpCallObs() {
+    private static Observable<String> getFromHttp() {
         return Observable.fromCallable(() -> {
             log.debug("Sent HTTP Request. Waiting for response");
             ConcurrencyUtil.sleep(1000);
